2bdf196241cdeb5b6218cfb40ca409341130cb20
Anurag <srivastava.anurag1989@gmail.com>
2024-07-30 14:09:20 +0530

    Modified cart test page

--- src/tests/CartPageTest.java
+++ src/tests/CartPageTest.java
@@ -5,6 +5,8 @@ public class CartPageTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Cart PAge Test");
+ 
+ System.out.println("Hello");
 	}
 
 }
